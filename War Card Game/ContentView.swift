af4099157022764e37808d9b4f2db371b1a6b7ed
Muhammadu Amhar <75868135+thecodemonster1@users.noreply.github.com>
2024-07-08 02:21:05 +0530

    final complete commit of this project

--- War Card Game/ContentView.swift
+++ War Card Game/ContentView.swift
@@ -8,14 +8,88 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var playerCard = "card7"
+    @State var cpuCard = "card13"
+    
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Image("background-cloth")
+                .resizable()
+                .ignoresSafeArea()
+            VStack{
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+//                var i = 0
+////                Image("button")
+//                Button("Deal"){
+//                    deal()
+////                    i = i + 1
+////                    print("Button is clicked",i,"times")
+//                }
+                
+                Button {
+                    deal()
+                } label: {
+                    Image("button")
+                }
+
+                Spacer()
+                HStack{
+                    Spacer()
+                    VStack{
+                        Text("Player")
+                            .font(.headline)
+                            .padding()
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack{
+                        Text("CPU")
+                            .font(.headline)
+                            .padding()
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                .padding(.bottom)
+                .foregroundColor(Color.white)
+                Spacer()
+            }
         }
-        .padding()
+    }
+    
+    func deal(){
+        // rendomize the player card
+        var playerCardValue = Int.random(in: 2...14) // actually range is 2 - 13 (not include 14)
+        var cpuCardValue = Int.random(in: 2...14)
+        playerCard = "card" + String(playerCardValue)
+        
+        // rendomize the cpu card
+        cpuCard = "card" + String(cpuCardValue)
+        
+        // update the scores
+        if playerCardValue > cpuCardValue {
+            playerScore += 1
+        }else if playerCardValue < cpuCardValue {
+            cpuScore += 1
+        }else{
+            print("Tie")
+        }
     }
 }
 
