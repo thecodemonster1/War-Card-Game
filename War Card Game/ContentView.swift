54db0e81b65a9b904230796e3c5a90904d9b8024
Muhammadu Amhar <75868135+thecodemonster1@users.noreply.github.com>
2024-07-07 15:05:54 +0530

    Initial Commit

---
+++ War Card Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  War Card Game
+//
+//  Created by Mohamed Amhar on 7/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Mohamed Amhar on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
